d84b9268330a51e89f68f47cef4cc73fc21795f2
ajay_k_14 <aky.ajayk@gmail.com>
2026-09-16 20:52:32 +0530

    new commi

--- database/db.py
+++ database/db.py
@@ -129,7 +129,7 @@ def init_db():
     db = get_db()
     try:
         if db.query(User).count() == 0:
-            demo = User(name='Demo Driver', email='123@gmail.com', phone='+91-1234567890')
+            demo = User(name='Demo Driver', email='123@gmail.com', phone='+91-9000000000')
             demo.set_password('12345678')
             db.add(demo)
             db.commit()
